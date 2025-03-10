8aa29958e8e3438ef6494aa993af57ded9bf8762
wintein <156084862+wintein@users.noreply.github.com>
2025-03-10 14:56:45 +0300

    Add files via upload

---
+++ repository/test/test/FileName.cpp
@@ -0,0 +1,72 @@
+//Программа для решения задания работы 7
+#include <iostream> // cin, cout
+#include <iomanip> // setw
+#include <ctime> // rand
+
+using namespace std;
+
+void create_array(double**& matrix, int n) {
+    if (matrix) { 
+        for (int i = 0; i < n; i++)
+            delete[]matrix[i];
+        delete[]matrix;
+    }
+
+    matrix = new double* [n]; 
+    for (int i = 0; i < n; i++) 
+        matrix[i] = new double[n];
+}
+
+void fill_array(double** matrix, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            matrix[i][j] = int((rand() * 101.0 / RAND_MAX - 50.0) * 100 + 0.5) / 100.0;
+        }
+    }
+}
+
+void print_array(double** matrix, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << setw(8) << matrix[i][j];
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void change_array(double** matrix, int n) {
+    for (int i = 0; i < n - 1; i++)
+    {
+        swap(matrix[n - 1][i], matrix[i][n - 1]);
+    }
+}
+
+void delete_array(double** matrix, int n) {
+    for (int i = 0; i < n; i++) {
+        delete[]matrix[i];
+    }
+    delete[]matrix; //Освобождение памяти, выделенной под массив указателей
+    matrix = NULL; //Очистка указателя
+}
+
+int main()
+{
+    setlocale(0, "");
+    srand(time(NULL));
+
+    int n;
+    double** matrix = nullptr; //Объявление указателя на массив указателей
+    cout << "Введите порядок матрицы: ";
+    cin >> n;
+    cout << endl;
+    create_array(matrix, n);
+    cout << "------------Исходная матрица:------------" << endl;
+    fill_array(matrix, n);
+    print_array(matrix, n);
+    change_array(matrix, n);
+    cout << "------------Измененная матрица:------------" << endl;
+    print_array(matrix, n);
+    delete_array(matrix, n);
+    return 0;
+}
